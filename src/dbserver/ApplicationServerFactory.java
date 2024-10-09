00b0e533d2f355b62cb5aa7682fced559b862521
Ser090 <programacion.software.00@gmail.com>
2024-10-09 12:07:05 +0200

    Terminar main y la factory 2

--- src/dbserver/ApplicationServerFactory.java
+++ src/dbserver/ApplicationServerFactory.java
@@ -5,10 +5,32 @@
  */
 package dbserver;
 
+import java.net.Socket;
+import server.Worker;
+
 /**
  *
- * @author 2dam
+ * @author Sergio
  */
 public class ApplicationServerFactory {
+
+    private static ApplicationServerFactory instance;
+    private PostgresConnectionPool pool;
+
+    public ApplicationServerFactory() {
+        pool = new PostgresConnectionPool(10);
+    }
+
+    public static synchronized ApplicationServerFactory getInstance() {
+        if (instance == null) {
+            instance = new ApplicationServerFactory();
+        }
+        return instance;
+    }
+
+    public Worker crearWorker(Socket clienteSocket) {
+        Dao dao = new Dao(pool);
+        return new Worker(clienteSocket, dao);
+    }
 
 }
